b25e0f3395af716e716a0be6bf5e6a2124801ffb
Sergio Costa <tp.sergio.willames@totvspartners.com.br>
2023-02-24 04:45:35 -0300

    fake objects created for the tests.

---
+++ src/test/java/com/sw/apiperson/FixtureHelper.java
@@ -0,0 +1,11 @@
+package com.sw.apiperson;
+
+public class FixtureHelper {
+
+	public static final long MIN_RANGE_ID = 1L;
+	public static final long MAX_RANGE_ID = 200L;
+
+	public static final int MIN_RANGE_NUMBER = 1;
+	public static final int MAX_RANGE_NUMBER = 2000;
+
+}
